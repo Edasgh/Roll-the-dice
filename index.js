9902c19b78c8f0c933b4efb7f367d9f6ef32cbe6
Eshita <edas25564@gmail.com>
2023-02-13 18:42:35 +0530

    Roll the dice game

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+const Rolldice=()=>{ 
+        var randomnum1= Math.floor(Math.random()*6) + 1;
+        var randomimg1= "dice"+randomnum1+".png";
+        var randImgsrc1="/Project4/img/"+randomimg1;
+        console.log(randImgsrc1);
+        
+        var img1=document.getElementsByTagName('img')[0]
+        img1.src=("src" , randImgsrc1);
+        //img1.setAttribute=("src" , randImgsrc1); THIS SYNTAX DIDN'T WORK
+ 
+
+       //For second image
+       var randomnum2= Math.floor(Math.random()*6) + 1;
+       var randomimg2= "dice"+randomnum2+".png";
+       var randImgsrc2="/Project4/img/"+randomimg2;
+       
+       var img2=document.getElementsByTagName('img')[1]
+       img2.src=("src" , randImgsrc2);
+       
+
+    //Win or Draw function
+    if(randomnum1>randomnum2){
+        document.querySelector("h1").innerHTML="&#128681;Player 1 Wins"
+    }else if(randomnum2>randomnum1){
+        document.querySelector("h1").innerHTML="Player 2 Wins&#128681;"
+    }else{
+        document.querySelector("h1").innerHTML="Match Draw!"
+    }
+
+}
+
+
